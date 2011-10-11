50a96eb8505826c962cca68326bbba7413102dbf
Alexandre Lalonde <lalonde.alexandre@solutionsmodestes.com>
2011-10-11 11:17:35 -0400

    committing index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENT - REPO TEST
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
